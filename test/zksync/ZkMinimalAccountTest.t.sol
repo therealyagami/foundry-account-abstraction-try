51e14512ba25ac671cebea4526728b78c260a42f
yagamibrown <marveymillie20@gmail.com>
2024-09-16 12:01:16 -0400

    Initial commit

---
+++ test/zksync/ZkMinimalAccountTest.t.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {ZkMinimalAccount} from "src/zksync/ZkMinimalAccount.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+import {
+    Transaction,
+    MemoryTransactionHelper
+} from "lib/foundry-era-contracts/src/system-contracts/contracts/libraries/MemoryTransactionHelper.sol";
+
+contract ZkMinimalAccountTest is Test {
+    ZkMinimalAccount minimalAccount;
+    ERC20Mock usdc;
+
+    uint256 constant AMOUNT = 1e18;
+    bytes32 constant EMPTY_BYTES32 = bytes32(0);
+    address constant ANVIL_DEFAULT_ACCOUNT = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+
+    function setUp() public {
+        minimalAccount = new ZkMinimalAccount();
+        minimalAccount.transferOwnership(ANVIL_DEFAULT_ACCOUNT);
+        usdc = new ERC20Mock();
+        vm.deal(address(minimalAccount), AMOUNT);
+    }
+
+    function testZkOwnerCanExecuteCommands() public {
+        // Arrange
+        address dest = address(usdc);
+        uint256 value = 0;
+        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+        Transaction memory transaction =
+            _createUnsignedTransaction(minimalAccount.owner(), 113, dest, value, functionData);
+
+        // Act
+
+        vm.prank(minimalAccount.owner());
+
+        minimalAccount.executeTransaction(EMPTY_BYTES32, EMPTY_BYTES32, transaction);
+        console2.log(minimalAccount.owner());
+        // Assert
+        assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+    }
+    /*//////////////////////////////////////////////////////////////
+                                HELPERS
+    //////////////////////////////////////////////////////////////*/
+
+    function _createUnsignedTransaction(
+        address from,
+        uint8 transactionType,
+        address to,
+        uint256 value,
+        bytes memory data
+    ) internal view returns (Transaction memory) {
+        uint256 nonce = vm.getNonce(address(minimalAccount));
+        bytes32[] memory factoryDeps = new bytes32[](0);
+        return Transaction({
+            txType: transactionType, // type 113 (0x71).
+            from: uint256(uint160(from)), // address => uint256 because addresses < 32 bytes.
+            to: uint256(uint160(to)),
+            gasLimit: 16777216,
+            gasPerPubdataByteLimit: 16777216,
+            maxFeePerGas: 16777216,
+            maxPriorityFeePerGas: 16777216,
+            paymaster: 0,
+            nonce: nonce,
+            value: value,
+            reserved: [uint256(0), uint256(0), uint256(0), uint256(0)],
+            data: data,
+            signature: hex"",
+            factoryDeps: factoryDeps,
+            paymasterInput: hex"",
+            reservedDynamic: hex""
+        });
+    }
+}
